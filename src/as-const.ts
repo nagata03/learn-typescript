385ceaee2a243b76e55aa159e8b0145f890bf73d
nagata03 <nagata.job@gmail.com>
2020-11-22 07:09:39 +0900

    learn as const

---
+++ src/as-const.ts
@@ -0,0 +1,15 @@
+export {};
+
+let name = 'Sho';
+name = 'shooen';
+
+let nickname = 'shooen' as const;
+//nickname = 'shooyan';
+
+let profile = {
+  name: 'Sho',
+  height: 178,
+} as const;
+
+//profile.name = 'shooen';
+//profile.height = 180;
